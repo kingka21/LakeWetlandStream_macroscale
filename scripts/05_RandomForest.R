ce97baf1626d84944f2c1005e948466145e10e4b
kingka21 <kingka21@msu.edu>
2019-05-15 17:32:13 -0400

    update random forest for more efficient code

--- scripts/05_RandomForest.R
+++ scripts/05_RandomForest.R
@@ -17,57 +17,95 @@ allecos<-gtools::smartbind(lake, stream, wetland)
 
 ### Compare biotic and abiotic variables to predictor variables###
 ## Random forest procedure
-### TP #### 
-#all 
-set.seed(8) #TRY DIFFERENT SEED SETS 188, 18, 999 top predictors don't change 
+#set up parameters 
 ntreez <- 500
+par(mfrow=c(2,2))
+par(mar=c(2,2,1,1), oma=c(2,2,0,0))
+set_string <-c(8, 188, 18, 999) #TRY DIFFERENT SEED SETS 188, 18, 999 top predictors don't change 
+
+### TP #### 
 noNAs<-allecos[!(is.na(allecos$TP)),] 
-RF_TP <- randomForest(log(TP+1) ~ Rveg + ELEVMEAN + Type + DEPTH + POPDEN + ROADDEN + NDEP + 
-                        TMIN + TMAX + PrecipNorm + PrecipSummer + Tsummer + PrecipWinter + AG_PCT + FOREST_PCT +
-                        WETLAND_PCT + URBAN_PCT + SHRUB_GRASS_PCT + AGGR_ECO9_2015, 
-                      data=noNAs, ntree=ntreez, importance=T, na.action=na.omit)
-RF_TP
-TP_imp <-randomForest::importance(RF_TP, type=1, scale=FALSE) #mean decrease in accuracy (also called permutation accuracy importance).
+for (i in 1:length(set_string)) {
+  set.seed(set_string[i])
+  RF_TP <- randomForest(log(TP+1) ~ Rveg + ELEVMEAN + Type + DEPTH + POPDEN + ROADDEN + NDEP + 
+                          TMIN + TMAX + PrecipNorm + PrecipSummer + Tsummer + PrecipWinter + AG_PCT + FOREST_PCT +
+                          WETLAND_PCT + URBAN_PCT + SHRUB_GRASS_PCT + AGGR_ECO9_2015, 
+                        data=noNAs, ntree=ntreez, importance=T, na.action=na.omit)
+  RF_TP
+  TP_imp <-randomForest::importance(RF_TP, type=1, scale=FALSE) #mean decrease in accuracy (also called permutation accuracy importance).
+  imp<-as.data.frame(TP_imp) 
+  imp$'Pred'   <-rownames(imp)
+  imp <- structure(imp$`%IncMSE`, names = as.character(imp$Pred))
+  
+  dotchart(imp[order(imp)], xlab = "mean decrease in accuracy",
+                main = set_string[i])
+  
+} 
 
 # diagnostic for if I ran enough trees to reduce MSE  
 plot(RF_TP, xlim=c(0,500))
 
 #### TN ##### 
-set.seed(8) #TRY DIFFERENT SEED SETS 188, 18, 999, 8 
 noNAs<-allecos[!(is.na(allecos$TN)),] 
+for (i in 1:length(set_string)) { #TRY DIFFERENT SEED SETS 188, 18, 999, 8
+  set.seed(set_string[i])
 RF_TN <- randomForest(log(TN+1) ~ Rveg + ELEVMEAN + Type + DEPTH + POPDEN + ROADDEN + NDEP + 
                         TMIN + TMAX + PrecipNorm + PrecipSummer + Tsummer + PrecipWinter + AG_PCT + FOREST_PCT +
                         WETLAND_PCT + URBAN_PCT + SHRUB_GRASS_PCT + AGGR_ECO9_2015, 
                       data=noNAs, ntree=ntreez, importance=T, na.action=na.omit)
 RF_TN
 TN_imp <-randomForest::importance(RF_TN, type=1, scale=FALSE) #mean decrease in accuracy (also called permutation accuracy importance).
+imp<-as.data.frame(TN_imp) 
+imp$'Pred'   <-rownames(imp)
+imp <- structure(imp$`%IncMSE`, names = as.character(imp$Pred))
+
+dotchart(imp[order(imp)], xlab = "mean decrease in accuracy",
+         main = set_string[i])
+
+} 
 
 # diagnostic for if I ran enough trees to reduce MSE  
 plot(RF_TN, xlim=c(0,600))
 
 #### ChlA ##### 
-set.seed(8) #TRY DIFFERENT SEED SETS 999, 188, 18, 8 
 noNAs<-allecos[!(is.na(allecos$CHLA)),] 
+for (i in 1:length(set_string)) { #TRY DIFFERENT SEED SETS 188, 18, 999, 8
+  set.seed(set_string[i])
 RF_CHLA <- randomForest(log(CHLA+1) ~ Rveg + ELEVMEAN + Type + DEPTH + POPDEN + ROADDEN + NDEP + 
                         TMIN + TMAX + PrecipNorm + PrecipSummer + Tsummer + PrecipWinter + AG_PCT + FOREST_PCT +
                         WETLAND_PCT + URBAN_PCT + SHRUB_GRASS_PCT + AGGR_ECO9_2015, 
                       data=noNAs, ntree=ntreez, importance=T, na.action=na.omit)
 RF_CHLA
 CHLA_imp <-randomForest::importance(RF_CHLA, type=1, scale=FALSE) #mean decrease in accuracy (also called permutation accuracy importance).
+imp<-as.data.frame(CHLA_imp) 
+imp$'Pred'   <-rownames(imp)
+imp <- structure(imp$`%IncMSE`, names = as.character(imp$Pred))
 
+dotchart(imp[order(imp)], xlab = "mean decrease in accuracy",
+         main = set_string[i])
+
+} 
 # diagnostic for if I ran enough trees to reduce MSE  
 plot(RF_CHLA, xlim=c(0,600))
 
 #### Aquatic Veg ##### 
-set.seed(8) #TRY DIFFERENT SEED SETS 999, 188, 18, 8 
 noNAs<-allecos[!(is.na(allecos$aqveg)),] 
+for (i in 1:length(set_string)) { #TRY DIFFERENT SEED SETS 188, 18, 999, 8
+  set.seed(set_string[i])
 RF_aqveg <- randomForest(log(aqveg+1) ~ Rveg + ELEVMEAN + Type + DEPTH + POPDEN + ROADDEN + NDEP + 
                           TMIN + TMAX + PrecipNorm + PrecipSummer + Tsummer + PrecipWinter + AG_PCT + FOREST_PCT +
                           WETLAND_PCT + URBAN_PCT + SHRUB_GRASS_PCT + AGGR_ECO9_2015, 
                         data=noNAs, ntree=ntreez, importance=T, na.action=na.omit)
 RF_aqveg
 veg_imp <-randomForest::importance(RF_aqveg, type=1, scale=FALSE) #mean decrease in accuracy (also called permutation accuracy importance).
+imp<-as.data.frame(veg_imp) 
+imp$'Pred'   <-rownames(imp)
+imp <- structure(imp$`%IncMSE`, names = as.character(imp$Pred))
 
+dotchart(imp[order(imp)], xlab = "mean decrease in accuracy",
+         main = set_string[i])
+
+} 
 # diagnostic for if I ran enough trees to reduce MSE  
 plot(RF_aqveg, xlim=c(0,600))
 
@@ -168,24 +206,35 @@ allecos_s$lnCHL<-log(allecos_s$CHLA+1)
 allecos_s$lnaqveg<-log(allecos_s$aqveg+1)
 
 
-  set.seed(8)  # run with each variable 
-  RF <- randomForest(lnaqveg ~ Rveg + ELEVMEAN + Type + DEPTH + POPDEN + ROADDEN + NDEP + 
-                             TMIN + TMAX + PrecipNorm + PrecipSummer + Tsummer + PrecipWinter + AG_PCT + FOREST_PCT +
-                             WETLAND_PCT + URBAN_PCT + SHRUB_GRASS_PCT + AGGR_ECO9_2015, 
-                           data=allecos_s, ntree=ntreez, importance=T, na.action=na.omit)
-  print(RF)
-  s_imp <-randomForest::importance(RF, type=1, scale=FALSE) 
-  imp<-as.data.frame(s_imp)  
-  imp$'Pred'   <-rownames(imp)
-  #imp <- structure(imp$`%IncMSE`, names = as.character(imp$Pred))
-  io<-imp[order(imp$`%IncMSE`),]
-  io$`Pred` <- factor(io$`Pred`, levels=io$`Pred`)  # convert to factor to retain sorted order in plot.
-  
 #plot importance 
   library(ggplot2)
   theme_set(theme_bw())  
 
 #TP
+  
+  for (i in 1:length(set_string)) { #TRY DIFFERENT SEED SETS 188, 18, 999, 8
+    set.seed(set_string[i])
+    s_TP <- randomForest(lnTP ~ Rveg + ELEVMEAN + Type + DEPTH + POPDEN + ROADDEN + NDEP + 
+                           TMIN + TMAX + PrecipNorm + PrecipSummer + Tsummer + PrecipWinter + AG_PCT + FOREST_PCT +
+                           WETLAND_PCT + URBAN_PCT + SHRUB_GRASS_PCT + AGGR_ECO9_2015, 
+                         data=allecos_s, ntree=ntreez, importance=T, na.action=na.omit)
+    print(s_TP)
+    s_imp <-randomForest::importance(s_TP, type=1, scale=FALSE) 
+    imp<-as.data.frame(s_imp) 
+    imp$'Pred'   <-rownames(imp)
+    imp <- structure(imp$`%IncMSE`, names = as.character(imp$Pred))
+    
+    dotchart(imp[order(imp)], xlab = "mean decrease in accuracy",
+             main = set_string[i])
+    
+  } 
+
+s_imp <-randomForest::importance(s_TP, type=1, scale=FALSE) 
+imp<-as.data.frame(s_imp)  
+imp$'Pred'   <-rownames(imp)
+io<-imp[order(imp$`%IncMSE`),]
+io$`Pred` <- factor(io$`Pred`, levels=io$`Pred`)  # convert to factor to retain sorted order in plot.
+  
 TP<- ggplot(io, aes(x=`Pred`, y=`%IncMSE`)) + 
     geom_point(stat="identity", size=2) + 
     coord_flip(ylim = c(0, 0.6)) + 
@@ -197,6 +246,29 @@ TP<- ggplot(io, aes(x=`Pred`, y=`%IncMSE`)) +
     theme(axis.title.y=element_blank())
 
 #TN
+for (i in 1:length(set_string)) { #TRY DIFFERENT SEED SETS 188, 18, 999, 8
+  set.seed(set_string[i])
+  s_TN <- randomForest(lnTN ~ Rveg + ELEVMEAN + Type + DEPTH + POPDEN + ROADDEN + NDEP + 
+                         TMIN + TMAX + PrecipNorm + PrecipSummer + Tsummer + PrecipWinter + AG_PCT + FOREST_PCT +
+                         WETLAND_PCT + URBAN_PCT + SHRUB_GRASS_PCT + AGGR_ECO9_2015, 
+                       data=allecos_s, ntree=ntreez, importance=T, na.action=na.omit)
+  print(s_TN)
+  s_imp <-randomForest::importance(s_TN, type=1, scale=FALSE) 
+  imp<-as.data.frame(s_imp) 
+  imp$'Pred'   <-rownames(imp)
+  imp <- structure(imp$`%IncMSE`, names = as.character(imp$Pred))
+  
+  dotchart(imp[order(imp)], xlab = "mean decrease in accuracy",
+           main = set_string[i])
+  
+} 
+
+s_imp <-randomForest::importance(s_TN, type=1, scale=FALSE) 
+imp<-as.data.frame(s_imp)  
+imp$'Pred'   <-rownames(imp)
+io<-imp[order(imp$`%IncMSE`),]
+io$`Pred` <- factor(io$`Pred`, levels=io$`Pred`)  # convert to factor to retain sorted order in plot.
+
 TN<- ggplot(io, aes(x=`Pred`, y=`%IncMSE`)) + 
   geom_point(stat="identity", size=2) + 
   coord_flip(ylim = c(0, 0.6)) + 
@@ -208,6 +280,29 @@ TN<- ggplot(io, aes(x=`Pred`, y=`%IncMSE`)) +
   theme(axis.title.y=element_blank())
 
 #CHL
+for (i in 1:length(set_string)) { #TRY DIFFERENT SEED SETS 188, 18, 999, 8
+  set.seed(set_string[i])
+  s_CHL <- randomForest(lnCHL ~ Rveg + ELEVMEAN + Type + DEPTH + POPDEN + ROADDEN + NDEP + 
+                         TMIN + TMAX + PrecipNorm + PrecipSummer + Tsummer + PrecipWinter + AG_PCT + FOREST_PCT +
+                         WETLAND_PCT + URBAN_PCT + SHRUB_GRASS_PCT + AGGR_ECO9_2015, 
+                       data=allecos_s, ntree=ntreez, importance=T, na.action=na.omit)
+  print(s_CHL)
+  s_imp <-randomForest::importance(s_CHL, type=1, scale=FALSE) 
+  imp<-as.data.frame(s_imp) 
+  imp$'Pred'   <-rownames(imp)
+  imp <- structure(imp$`%IncMSE`, names = as.character(imp$Pred))
+  
+  dotchart(imp[order(imp)], xlab = "mean decrease in accuracy",
+           main = set_string[i])
+  
+} 
+
+s_imp <-randomForest::importance(s_CHL, type=1, scale=FALSE) 
+imp<-as.data.frame(s_imp)  
+imp$'Pred'   <-rownames(imp)
+io<-imp[order(imp$`%IncMSE`),]
+io$`Pred` <- factor(io$`Pred`, levels=io$`Pred`)  # convert to factor to retain sorted order in plot.
+
 CHL<- ggplot(io, aes(x=`Pred`, y=`%IncMSE`)) + 
   geom_point(stat="identity", size=2) + 
   coord_flip() + 
@@ -220,13 +315,36 @@ CHL<- ggplot(io, aes(x=`Pred`, y=`%IncMSE`)) +
 
 
 #AqVeg
+for (i in 1:length(set_string)) { #TRY DIFFERENT SEED SETS 188, 18, 999, 8
+  set.seed(set_string[i])
+  s_AQ <- randomForest(lnaqveg ~ Rveg + ELEVMEAN + Type + DEPTH + POPDEN + ROADDEN + NDEP + 
+                         TMIN + TMAX + PrecipNorm + PrecipSummer + Tsummer + PrecipWinter + AG_PCT + FOREST_PCT +
+                         WETLAND_PCT + URBAN_PCT + SHRUB_GRASS_PCT + AGGR_ECO9_2015, 
+                       data=allecos_s, ntree=ntreez, importance=T, na.action=na.omit)
+  print(s_AQ)
+  s_imp <-randomForest::importance(s_AQ, type=1, scale=FALSE) 
+  imp<-as.data.frame(s_imp) 
+  imp$'Pred'   <-rownames(imp)
+  imp <- structure(imp$`%IncMSE`, names = as.character(imp$Pred))
+  
+  dotchart(imp[order(imp)], xlab = "mean decrease in accuracy",
+           main = set_string[i])
+  
+} 
+
+s_imp <-randomForest::importance(s_AQ, type=1, scale=FALSE) 
+imp<-as.data.frame(s_imp)  
+imp$'Pred'   <-rownames(imp)
+io<-imp[order(imp$`%IncMSE`),]
+io$`Pred` <- factor(io$`Pred`, levels=io$`Pred`)  # convert to factor to retain sorted order in plot.
+
 veg<- ggplot(io, aes(x=`Pred`, y=`%IncMSE`)) + 
     geom_point(stat="identity", size=2) + 
     coord_flip( ylim = c(0, 0.8)) + 
     theme(axis.text.y=element_text(face = c('plain', 'plain', 'plain',
                                             'plain', 'plain', 'plain', 'plain', 'plain', 'plain',
                                             'plain', 'plain', 'plain', 'plain', 'plain', 'plain', 
-                                            'bold', 'bold', 'plain',  'bold'))) +
+                                            'plain', 'plain', 'plain',  'bold'))) +
     ylab("mean decrease in accuracy") + 
     theme(axis.title.y=element_blank())
   
